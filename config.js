9e3341a333222337c189858af61422d3fa8d913e
GuoYingxu <33899027+GuoYingxu@users.noreply.github.com>
2021-10-27 21:30:05 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const config = {
+  test:1,
   // API_HOST:'http://localhost:3001/api' ,
   API_HOST: process.env.NODE_ENV === 'development' ? 'http://localhost:80' : '',
   UPLOAD_HOST:"https://up-z2.qbox.me",
